26c4e02c1d6063ab81b47d0051586d7acd48db69
Juan <fersot100@yahoo.com>
2017-02-14 21:01:22 -0800

    Minor Corrections

--- morse-app.js
+++ morse-app.js
@@ -1,7 +1,7 @@
 var morseApp = angular.module('morse-app', []);
 
 morseApp.controller('tablectrl' , function($scope, $http){
-	$http.get('morseTable.json').success(function (res) {
-		$scope.translations = res; // Pipe the 
+	$http.get('morseTable.json').success(function (res) { 
+		$scope.translations = JSON.parse(res); // Pipe the translations into an array object
 	});
 });
